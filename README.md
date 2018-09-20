bbf7c4b4a9e5ccf8ebc54520375337b722e37dec
kluuker <l@kluuker.nl>
2018-09-20 13:36:45 +0200

    updated bug data

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # bug-url-query-param
 
-when sending the following url ?=bug every parameter will get filled with the string "bug"
+when sending the following url **?=bug** every parameter will get filled with the string "bug"
+(except those with the `FromQuery` attribute that has the `Name=` property specified)
 
-![bug example](bug-example.png)
+![bug example ?=bug](example-bug.png)
+
+sending a empty pair like **?=** results in
+
+![bug example ?=](example-empty-kvp.png)
+
+sending just a **?** is normal
+
+![bug example ?](example-questionmark.png)
--- url-query-param-bug/Controllers/HomeController.cs
+++ url-query-param-bug/Controllers/HomeController.cs
@@ -4,21 +4,19 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using url_query_param_bug.Models;
 
 namespace url_query_param_bug.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index(string a, string b, string c)
+        public IActionResult Index(
+            string a, 
+            string b,
+            [FromQuery]string c, 
+            [FromQuery(Name = "d")]string d,
+            string e)
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
